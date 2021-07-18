dbeeb96363a0b4a516b7c2294011f49a0bec6614
chrisgolds <chris.golds@hotmail.com>
2021-07-18 23:36:31 +0100

    Experimenting table outputs

--- server.R
+++ server.R
@@ -36,6 +36,56 @@ server <- function(input, output) {
   output$group_e_table <- renderTable(GROUP_TABLES$E)
   output$group_f_table <- renderTable(GROUP_TABLES$F)
   
+  result <- function(team_a, score_a, team_b, score_b, table) {
+    
+    print("Here")
+    
+    if (score_a > score_b) {
+      table[team_a,"Points"] <- as.integer(table[team_a,"Points"]) + 3
+    } else if (score_b > score_a) {
+      table[team_b,"Points"] <- as.integer(table[team_b,"Points"]) + 3
+    }
+    
+    return(table)
+    
+  }
   
+  observe({
+    
+    # Group A
+    output$group_a_table <- renderTable(result(group_a[1],
+                                               eval(parse(text=paste0("input$",group_a[1],"_first"))),
+                                               group_a[2],
+                                               eval(parse(text=paste0("input$",group_a[2],"_first"))),
+                                               GROUP_TABLES$A))
+    output$group_a_table <- renderTable(result(group_a[3],
+                                               eval(parse(text=paste0("input$",group_a[3],"_first"))),
+                                               group_a[4],
+                                               eval(parse(text=paste0("input$",group_a[4],"_first"))),
+                                               GROUP_TABLES$A))
+    
+    # output$group_a_table <- renderTable(result(group_a[1],
+    #                                            eval(parse(text=paste0("input$",group_a[1],"_second"))),
+    #                                            group_a[3],
+    #                                            eval(parse(text=paste0("input$",group_a[3],"_second"))),
+    #                                            GROUP_TABLES$A))
+    # output$group_a_table <- renderTable(result(group_a[2],
+    #                                            eval(parse(text=paste0("input$",group_a[2],"_second"))),
+    #                                            group_a[4],
+    #                                            eval(parse(text=paste0("input$",group_a[4],"_second"))),
+    #                                            GROUP_TABLES$A))
+    # 
+    # output$group_a_table <- renderTable(result(group_a[4],
+    #                                            eval(parse(text=paste0("input$",group_a[4],"_third"))),
+    #                                            group_a[1],
+    #                                            eval(parse(text=paste0("input$",group_a[1],"_third"))),
+    #                                            GROUP_TABLES$A))
+    # output$group_a_table <- renderTable(result(group_a[2],
+    #                                            eval(parse(text=paste0("input$",group_a[2],"_third"))),
+    #                                            group_a[3],
+    #                                            eval(parse(text=paste0("input$",group_a[3],"_third"))),
+    #                                            GROUP_TABLES$A))
+    
+  })
   
 }
